6309de54293140fa9514fca736a7c11b284acab1
klickstarter <git@rgb10.de>
2015-02-01 02:21:22 +0100

    Some minor refactoring. Added prefix option.

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 var fs = require('fs');
 var path = require('path');
-var mustache = require('mustache');
-var through = require('through');
 var gutil = require('gulp-util');
-var mime = require('mime');
+var through = require('through');
+var mustache = require('mustache');
 var sizeOf = require('image-size');
+var mime = require('mime');
 var md5 = require('MD5');
 
 module.exports = function (options) {
     var buffer = [];
-    if(!options.template){
+
+    if (!options) options = {};
+
+    if(!options.template)
         options.template = path.join(__dirname, 'compass-imagehelper.mustache');
-    }
-    if(!options.targetFile){
+
+    if(!options.targetFile)
         options.targetFile = '_compass-imagehelper.scss';
-    }
-    var tpl = fs.readFileSync(options.template).toString();
+
+    if(!options.prefix)
+        options.prefix = '';
+
+    var template = fs.readFileSync(options.template).toString();
 
 
     var bufferContents = function (file) {
+        //if (file.isNull()) {
+        //    this.push(file);
+        //    return callback();
+        //} else if (file.isStream()) {
+        //    this.emit("error", new gutil.PluginError("gulp-compass-imagehelper", "Stream content is not supported"));
+        //    return callback();
+        //}
 
-        var imgData = {};
+        var imageInfo = {};
         var data;
         var encoding;
 
         var mimetype = mime.lookup(file.path);
         var dimensions = sizeOf(file.path);
 
-        imgData.width = dimensions.width;
-        imgData.height = dimensions.height;
-        imgData.type = dimensions.type;
-        imgData.basename = path.basename(file.path, '.' + dimensions.type);
-        imgData.path= path.relative(file.base, file.path);
-        imgData.fullname = imgData.relpath.replace('/', '-');
-        imgData.mime = mimetype;
-        imgData.hash = md5(file.contents);
+        imageInfo.width = dimensions.width;
+        imageInfo.height = dimensions.height;
+        imageInfo.type = dimensions.type;
+        imageInfo.basename = file.base;
+        imageInfo.path= path.relative(file.base, file.path);
+        imageInfo.fullname = imageInfo.path.split(path.sep).join('-');
+        imageInfo.mime = mimetype;
+        imageInfo.hash = md5(file.contents);
         if (mimetype == 'image/svg+xml') {
             data = encodeURIComponent(file.contents);
             encoding = 'utf8';
@@ -42,14 +55,15 @@ module.exports = function (options) {
             data = file.contents.toString('base64');
             encoding = 'base64';
         }
-        imgData.data = 'url(data:' + mimetype + ';' + encoding + ',' + data + ')';
-        buffer.push(imgData);
+        imageInfo.data = 'url(data:' + mimetype + ';' + encoding + ',' + data + ')';
+        buffer.push(imageInfo);
 
     };
 
     var endStream = function () {
         this.emit('data', new gutil.File({
-            contents: new Buffer(mustache.render(tpl, {
+            contents: new Buffer(mustache.render(template, {
+                prefix: options.prefix,
                 items: buffer
             }), 'utf8'),
             path: options.targetFile
