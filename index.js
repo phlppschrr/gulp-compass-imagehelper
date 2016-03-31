9ff7361ee34bdcee6b39833e6428e4941956f55b
Philipp Schreier <git@rgb10.de>
2016-03-31 15:53:44 +0200

    formatting

--- index.js
+++ index.js
@@ -11,106 +11,106 @@ var svgo = new SVGO();
 var appRoot = require('app-root-path').path;
 
 module.exports = function (options) {
-    var images = [];
-    //var images_path = options.images_path || '';
+  var images = [];
+  //var images_path = options.images_path || '';
 
-    if (!options) options = {};
+  if (!options) options = {};
 
-    if(!options.template)
-        options.template = path.join(__dirname, 'compass-imagehelper.mustache');
+  if (!options.template)
+      options.template = path.join(__dirname, 'compass-imagehelper.mustache');
 
-    if(!options.targetFile)
-        options.targetFile = '_compass-imagehelper.scss';
+  if (!options.targetFile)
+      options.targetFile = '_compass-imagehelper.scss';
 
-    if(!options.prefix)
-        options.prefix = '';
+  if (!options.prefix)
+      options.prefix = '';
 
+  var template = fs.readFileSync(options.template).toString();
 
-    var template = fs.readFileSync(options.template).toString();
+  var pathPrefix = function () {
+    var result = '';
+    if (options.http_images_path) {
+      result = options.http_images_path;
+    } else if (options.css_path && options.images_path) {
+      // relative path from css folder to images
+      result = path.relative(options.css_path, options.images_path);
+    } else if (options.images_path) {
+      // relative from project url
+      result = path.relative(appRoot, options.images_path);
+    }
 
-    var pathPrefix = function () {
-        var result = '';
-        if (options.http_images_path) {
-            result = options.http_images_path;
-        } else if (options.css_path && options.images_path) {
-            // relative path from css folder to images
-            result = path.relative(options.css_path, options.images_path);
-        } else if(options.images_path) {
-            // relative from project url
-            result = path.relative(appRoot, options.images_path);
-        }
+    // make sure pathPrefix ends with a trailing slash
+    if (result && result.substr(-1) != '/') {
+      result = result + '/';
+    }
 
-        // make sure pathPrefix ends with a trailing slash
-        if (result && result.substr(-1) != '/') {
-            result = result + '/';
-        }
-        return result;
-    };
+    return result;
+  };
 
+  var bufferContents = function (file) {
+    if (!options.images_path) {
+      // autodetect images_path with the first file
+      options.images_path = path.relative(appRoot,  file.base);
+    }
 
-    var bufferContents = function (file) {
-        if(!options.images_path) {
-            // autodetect images_path with the first file
-            options.images_path = path.relative( appRoot,  file.base);
-        }
+    var imageInfo = {};
+    var data;
+    var encoding;
 
-        var imageInfo = {};
-        var data;
-        var encoding;
+    var mimetype = mime.lookup(file.path);
+    var dimensions;
 
-        var mimetype = mime.lookup(file.path);
-        var dimensions;
+    if (mimetype == 'image/svg+xml') {
+      //data = encodeURIComponent(file.contents.toString());
+      //encoding = 'utf8';
+      data = file.contents.toString('base64');
+      encoding = 'base64';
+      try {
+        dimensions = sizeOf(file.path);
+      } catch (e) {
+        // could not read width/height from svg. Try again with the slower svgo parser:
+        svgo.optimize(file.contents.toString(), function (res) {
+          dimensions = {
+            width: res.info.width,
+            height: res.info.height,
+          };
+          console.log(res);
+        });
+      }
 
-        if (mimetype == 'image/svg+xml') {
-            //data = encodeURIComponent(file.contents.toString());
-            //encoding = 'utf8';
-            data = file.contents.toString('base64');
-            encoding = 'base64';
-            try{
-                dimensions = sizeOf(file.path);
-            } catch(e){
-                // could not read width/height from svg. Try again with the slower svgo parser:
-                svgo.optimize(file.contents.toString(), function(res) {
-                    dimensions = {
-                        width: res.info.width,
-                        height: res.info.height
-                    };
-                    console.log(res);
-                });
-            }
+    } else {
+      data = file.contents.toString('base64');
+      encoding = 'base64';
+      dimensions = sizeOf(file.path);
+    }
 
-        } else {
-            data = file.contents.toString('base64');
-            encoding = 'base64';
-            dimensions= sizeOf(file.path);
-        }
-        imageInfo.width = dimensions.width;
-        imageInfo.height = dimensions.height;
-        imageInfo.mime = mimetype;
-        imageInfo.filename = path.basename(file.path);
-        imageInfo.basename = path.basename(file.path, path.extname(file.path));
-        imageInfo.dirname = path.basename(path.dirname(file.path));
-        imageInfo.ext = path.extname(file.path);
-        imageInfo.path = path.relative(options.images_path, file.path);
-        // Replace /, \ and . with -
-        imageInfo.fullname = imageInfo.path.replace(/[\/\\\.]/g, '-');
-        imageInfo.hash = md5(file.contents);
-        imageInfo.data = 'url(data:' + mimetype + ';' + encoding + ',' + data + ')';
-        images.push(imageInfo);
+    imageInfo.width = dimensions.width;
+    imageInfo.height = dimensions.height;
+    imageInfo.mime = mimetype;
+    imageInfo.filename = path.basename(file.path);
+    imageInfo.basename = path.basename(file.path, path.extname(file.path));
+    imageInfo.dirname = path.basename(path.dirname(file.path));
+    imageInfo.ext = path.extname(file.path);
+    imageInfo.path = path.relative(options.images_path, file.path);
+    // Replace /, \ and . with -
+    imageInfo.fullname = imageInfo.path.replace(/[\/\\\.]/g, '-');
+    imageInfo.hash = md5(file.contents);
+    imageInfo.data = 'url(data:' + mimetype + ';' + encoding + ',' + data + ')';
+    images.push(imageInfo);
 
-    };
+  };
 
-    var endStream = function () {
-        this.emit('data', new gutil.File({
-            contents: new Buffer(mustache.render(template, {
-                prefix: options.prefix,
-                path_prefix: pathPrefix(),
-                items: images
-            }), 'utf8'),
-            path: options.targetFile
-        }));
-        this.emit('end');
-    };
+  var endStream = function () {
+    this.emit('data', new gutil.File({
+      contents: new Buffer(mustache.render(template, {
+        prefix: options.prefix,
+        path_prefix: pathPrefix(),
+        items: images,
+      }), 'utf8'),
+      path: options.targetFile,
+    }));
+    this.emit('end');
+  };
 
-    return new through(bufferContents, endStream);
+  return new through(bufferContents, endStream);
 };
